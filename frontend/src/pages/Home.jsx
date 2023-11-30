195ffee4f6acdb61848f0c27fcc65a828beeb434
dev-ahmedali <ahmedali69325@gmail.com>
2023-11-30 10:12:59 +0600

    added hero section ended

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,71 +3,151 @@ import React from "react";
 import heroImg01 from "../assets/images/hero-img01.png";
 import heroImg02 from "../assets/images/hero-img02.png";
 import heroImg03 from "../assets/images/hero-img03.png";
+import icon01 from "../assets/images/icon01.png";
+import icon02 from "../assets/images/icon02.png";
+import icon03 from "../assets/images/icon03.png";
+import { Link } from "react-router-dom";
+import {ArrowRight} from "lucide-react"
 
 const Home = () => {
   return (
-    <section className="hero__section pt-[60px] 2xl:h-[800px]">
-      <div className="container">
-        <div className="flex flex-col lg:flex-row gap-[90px] items-center justify-between">
-          {/* hero content */}
+    <>
+      <section className="hero__section pt-[60px] 2xl:h-[800px]">
+        <div className="container">
+          <div className="flex flex-col lg:flex-row gap-[90px] items-center justify-between">
+            {/* hero content */}
 
-          <div>
-            <div className="lg:w-[570px]">
-              <h1 className="text-[36px] leading-[46px] text-zinc-800 font-[800] md:text-[60px] md:leading-[70px]">
-                Lorem ipsum dolor sit amet, consectetur
-              </h1>
-              <p className="text__para">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga
-                provident repellat tenetur voluptate possimus velit. Adipisci
-                voluptatibus iste fugit atque nihil corporis quibusdam quis
-                error!
-              </p>
-
-              <button className="btn hover:bg-indigo-600">
-                Booking an Appointment
-              </button>
-            </div>
-
-            <div className="mt-[30px] lg:mt-[70px] flex flex-col lg:flex-row lg:items-center gap-5 lg:gap-[30px]">
-              <div>
-                <h2 className="text-[36px] leading-[56px] lg:text-[44px] lg:leading-[54px] font-[700] text-zinc-800">
-                  30+
-                </h2>
-                <span className="w-[100px] h-2 bg-yellow-500 rounded-full block mt-[-14px]"></span>
-                <p className="text__para">Years of Experience</p>
-              </div>
-
-              <div>
-                <h2 className="text-[36px] leading-[56px] lg:text-[44px] lg:leading-[54px] font-[700] text-zinc-800">
-                  15+
-                </h2>
-                <span className="w-[100px] h-2 bg-purple-500 rounded-full block mt-[-14px]"></span>
-                <p className="text__para">Clinic Location</p>
-              </div>
-
-              <div>
-                <h2 className="text-[36px] leading-[56px] lg:text-[44px] lg:leading-[54px] font-[700] text-zinc-800">
-                  100%
-                </h2>
-                <span className="w-[100px] h-2 bg-sky-500 rounded-full block mt-[-14px]"></span>
-                <p className="text__para">Patient Satisfaction</p>
-              </div>
-            </div>
-          </div>
-          {/* hero content */}
-          <div className="flex gap-[30px] justify-end">
             <div>
-              <img className="w-full" src={heroImg01} alt="" />
-            </div>
+              <div className="lg:w-[570px]">
+                <h1 className="text-[36px] leading-[46px] text-zinc-800 font-[800] md:text-[60px] md:leading-[70px]">
+                  Lorem ipsum dolor sit amet, consectetur
+                </h1>
+                <p className="text__para">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga
+                  provident repellat tenetur voluptate possimus velit. Adipisci
+                  voluptatibus iste fugit atque nihil corporis quibusdam quis
+                  error!
+                </p>
 
-            <div className="mt-[30px]">
-              <img src={heroImg02} alt="" className="w-full mb-[30px]" />
-              <img src={heroImg03} alt="" className="w-full" />
+                <button className="btn hover:bg-indigo-600">
+                  Booking an Appointment
+                </button>
+              </div>
+
+              <div className="mt-[30px] lg:mt-[70px] flex flex-col lg:flex-row lg:items-center gap-5 lg:gap-[30px]">
+                <div>
+                  <h2 className="text-[36px] leading-[56px] lg:text-[44px] lg:leading-[54px] font-[700] text-zinc-800">
+                    30+
+                  </h2>
+                  <span className="w-[100px] h-2 bg-yellow-500 rounded-full block mt-[-14px]"></span>
+                  <p className="text__para">Years of Experience</p>
+                </div>
+
+                <div>
+                  <h2 className="text-[36px] leading-[56px] lg:text-[44px] lg:leading-[54px] font-[700] text-zinc-800">
+                    15+
+                  </h2>
+                  <span className="w-[100px] h-2 bg-purple-500 rounded-full block mt-[-14px]"></span>
+                  <p className="text__para">Clinic Location</p>
+                </div>
+
+                <div>
+                  <h2 className="text-[36px] leading-[56px] lg:text-[44px] lg:leading-[54px] font-[700] text-zinc-800">
+                    100%
+                  </h2>
+                  <span className="w-[100px] h-2 bg-sky-500 rounded-full block mt-[-14px]"></span>
+                  <p className="text__para">Patient Satisfaction</p>
+                </div>
+              </div>
+            </div>
+            {/* hero content */}
+            <div className="flex gap-[30px] justify-end">
+              <div>
+                <img className="w-full" src={heroImg01} alt="" />
+              </div>
+
+              <div className="mt-[30px]">
+                <img src={heroImg02} alt="" className="w-full mb-[30px]" />
+                <img src={heroImg03} alt="" className="w-full" />
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+      {/* hero section end */}
+      <section className="container">
+        <div className="lg:w-[470px] mx-auto">
+          <h2 className="heading text-center">
+            Providing the best medical service
+          </h2>
+          <p className="text__para text-center">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magnam,
+            illum.
+          </p>
+        </div>
+        <div className="flex flex-wrap items-center flex-col md:flex-row gap-5 lg:gap-[30px] mt-[30px] lg:mt-[55px] justify-between">
+          <div className="grid grid-cols-2 lg:grid-cols-3 gap-5 lg:gap-[30px]mt-[30px] lg:mt-[55px]">
+            <div className="py-[30px] px-5">
+              <div className="flex items-center justify-center">
+                <img src={icon01} alt="" />
+              </div>
+              <div className="mt-[30px]">
+                <h2 className="text-[26px] leading-9 text-zinc-800 font-[700] text-center">
+                  Find a Doctor
+                </h2>
+                <p className="text-[16px] leading-7 text-zinc-500 font-[400] mt-4 text-center">
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                  Cumque sapiente quo rem, sequi id et.
+                </p>
+                <Link
+                  to="/doctors"
+                  className="w-[44px] h-[44px] rounded-full border border-solid border-zinc-900 mt-[30px] mx-auto flex items-center justify-center group hover:bg-indigo-500 hover:border-none">
+                    <ArrowRight className="group-hover:text-white w-6 h-5"/>
+                  </Link>
+              </div>
+            </div>
+            <div className="py-[30px] px-5">
+              <div className="flex items-center justify-center">
+                <img src={icon02} alt="" />
+              </div>
+              <div className="mt-[30px]">
+                <h2 className="text-[26px] leading-9 text-zinc-800 font-[700] text-center">
+                  Find a Location
+                </h2>
+                <p className="text-[16px] leading-7 text-zinc-500 font-[400] mt-4 text-center">
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                  Cumque sapiente quo rem, sequi id et.
+                </p>
+                <Link
+                  to="/doctors"
+                  className="w-[44px] h-[44px] rounded-full border border-solid border-zinc-900 mt-[30px] mx-auto flex items-center justify-center group hover:bg-indigo-500 hover:border-none">
+                    <ArrowRight className="group-hover:text-white w-6 h-5"/>
+                  </Link>
+              </div>
+            </div>
+            <div className="py-[30px] px-5">
+              <div className="flex items-center justify-center">
+                <img src={icon03} alt="" />
+              </div>
+              <div className="mt-[30px]">
+                <h2 className="text-[26px] leading-9 text-zinc-800 font-[700] text-center">
+                  Book Appointment
+                </h2>
+                <p className="text-[16px] leading-7 text-zinc-500 font-[400] mt-4 text-center">
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                  Cumque sapiente quo rem, sequi id et.
+                </p>
+                <Link
+                  to="/doctors"
+                  className="w-[44px] h-[44px] rounded-full border border-solid border-zinc-900 mt-[30px] mx-auto flex items-center justify-center group hover:bg-indigo-500 hover:border-none">
+                    <ArrowRight className="group-hover:text-white w-6 h-5"/>
+                  </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
   );
 };
 
