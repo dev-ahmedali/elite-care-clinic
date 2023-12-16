a6234e7a8cbb15afc80df6672a09f59e717f01d3
dev-ahmedali <ahmedali69325@gmail.com>
2023-12-16 22:15:19 +0600

    added feature section

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -10,6 +10,8 @@ import { Link } from "react-router-dom";
 import { ArrowRight } from "lucide-react";
 import About from "../components/About/About";
 import ServicesList from "../components/services/ServicesList";
+import featureImg from "../assets/images/feature-img.png"
+import videoIcon from "../assets/images/video-icon.png"
 
 const Home = () => {
   return (
@@ -161,6 +163,51 @@ const Home = () => {
         </div>
       </section>
       {/* service section end */}
+      <section>
+        <div className="container">
+          <div className="flex items-center justify-between flex-col lg:flex-row">
+            {/* feature content */}
+            <div className="xl:w-[670px]">
+              <h2 className="heading">
+                Get virtual treatment <br/> anytime
+              </h2>
+              <ul className="pt-4">
+                  <li className="text_para">
+                    1. Schedule the appointment directly
+                  </li>
+                  <li className="text_para">
+                    2. Lorem ipsum dolor sit amet consectetur.
+                  </li>
+                  <li className="text_para">
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium, quos.
+                  </li>
+              </ul>
+              <Link to="/">
+                <button className="btn">Learn More</button>
+              </Link>
+            </div>
+            {/* feature image */}
+            <div className="relative z-10 xl:w-[770px] flex justify-end mt-[50px] lg:mt-0">
+              <img src={featureImg} className="w-3/4" alt="" />
+              <div className="w-[150px] lg:w-[248px] bg-white absolute bottom-[50px] left-0 md:bottom-[100px] md:left-5 p-2 pb-3 lg:px-4 lg:pb-[26px] rounded-[10px]">
+                <div className="flex items-center justify-between">
+                  <div className="flex items-center gap-[6px] lg:gap-3">
+                    <p className="text-[10px] leading-[10px] lg:text-[14px] lg:leading-5 text-zinc-800 font-[600]">Tue, 24</p>
+                    <p className="text-[10px] leading-[10px] lg:text-[14px] lg:leading-5 text-zinc-500 font-[400]">10:00</p>
+                  </div>
+                  <span className="w-5 h-5 lg:w-[34px] lg:h-[34px] flex items-center justify-center bg-yellow-500 rounded py-1 px-[6px] lg:py-3 lg:px-[9px]">
+                      <img src={videoIcon} alt="" />
+                  </span>
+                </div>
+
+                <div className="w-[65px] lg:w-[96px] bg-[#CCF0F3] py-1 px-2 lg-py-[60px] lg:px-[10px] text-[8px] leading-[8px] lg:leading-4 text-blue-500 font-[500] mt-2 lg:mt-4">
+                    consultation
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   );
 };
